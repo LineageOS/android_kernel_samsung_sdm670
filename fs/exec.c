49a9d5ed190847c3b9bbd3a1096104a3967e1149
Bruno Martins <bgcngm@gmail.com>
2021-05-16 19:16:45 +0100

    ARM64: Add Samsung micro hypervisor feature from T725XXU2DUD1
    Change-Id: Icd3de9e20c7cf1e85220e5502bb1ca661c79cd04

--- fs/exec.c
+++ fs/exec.c
@@ -72,6 +72,16 @@
 #include <linux/defex.h>
 #endif
 
+#ifdef CONFIG_RKP_NS_PROT
+#include <linux/sec_bsp.h>
+#endif
+
+#ifdef CONFIG_RKP_KDP
+#define rkp_is_nonroot(x) ((x->cred->type)>>1 & 1)
+#ifdef CONFIG_LOD_SEC
+#define rkp_is_lod(x) ((x->cred->type)>>3 & 1)
+#endif /*CONFIG_LOD_SEC*/
+#endif /*CONFIG_RKP_KDP*/
 int suid_dumpable = 0;
 
 static LIST_HEAD(formats);
@@ -1049,6 +1059,11 @@ static int exec_mmap(struct mm_struct *mm)
 	activate_mm(active_mm, mm);
 	tsk->mm->vmacache_seqnum = 0;
 	vmacache_flush(tsk);
+#ifdef CONFIG_RKP_KDP
+	if(rkp_cred_enable){
+	uh_call(UH_APP_RKP,0x43,(u64)current_cred(), (u64)mm->pgd,0,0);
+	}
+#endif /*CONFIG_RKP_KDP*/
 	task_unlock(tsk);
 	if (old_mm) {
 		up_read(&old_mm->mmap_sem);
@@ -1255,6 +1270,57 @@ void __set_task_comm(struct task_struct *tsk, const char *buf, bool exec)
 	task_unlock(tsk);
 	perf_event_comm(tsk, exec);
 }
+#ifdef CONFIG_RKP_NS_PROT
+extern struct super_block *sys_sb;	/* pointer to superblock */
+extern struct super_block *odm_sb;	/* pointer to superblock */
+extern struct super_block *vendor_sb;	/* pointer to superblock */
+extern struct super_block *rootfs_sb;	/* pointer to superblock */
+
+static int kdp_check_sb_mismatch(struct super_block *sb) 
+{	
+	if(is_boot_recovery()) {
+		return 0;
+	}
+	if((sb != rootfs_sb) && (sb != sys_sb)
+		&& (sb != odm_sb) && (sb != vendor_sb)) {
+		return 1;
+	}
+	return 0;
+}
+static int invalid_drive(struct linux_binprm * bprm) 
+{
+	struct super_block *sb =  NULL;
+	struct vfsmount *vfsmnt = NULL;
+	
+	vfsmnt = bprm->file->f_path.mnt;
+	if(!vfsmnt || 
+		!rkp_ro_page((unsigned long)vfsmnt)) {
+		printk("\nInvalid Drive #%s# #%p#\n",bprm->filename, vfsmnt);
+		return 1;
+	} 
+	sb = vfsmnt->mnt_sb;
+
+	if(kdp_check_sb_mismatch(sb)) {
+		printk("\n Superblock Mismatch #%s# vfsmnt #%p#sb #%p:%p:%p:%p:%p#\n",
+					bprm->filename, vfsmnt, sb, rootfs_sb, sys_sb, odm_sb, vendor_sb);
+		return 1;
+	}
+
+	return 0;
+}
+#define RKP_CRED_SYS_ID 1000
+
+static int is_rkp_priv_task(void)
+{
+	struct cred *cred = (struct cred *)current_cred();
+
+	if(cred->uid.val <= (uid_t)RKP_CRED_SYS_ID || cred->euid.val <= (uid_t)RKP_CRED_SYS_ID ||
+		cred->gid.val <= (gid_t)RKP_CRED_SYS_ID || cred->egid.val <= (gid_t)RKP_CRED_SYS_ID ){
+		return 1;
+	}
+	return 0;
+}
+#endif
 
 int flush_old_exec(struct linux_binprm * bprm)
 {
@@ -1281,6 +1347,13 @@ int flush_old_exec(struct linux_binprm * bprm)
 	 * Release all of the old mmap stuff
 	 */
 	acct_arg_size(bprm, 0);
+#ifdef CONFIG_RKP_NS_PROT
+	if(rkp_cred_enable &&
+		is_rkp_priv_task() && 
+		invalid_drive(bprm)) {
+		panic("\n KDP_NS_PROT: Illegal Execution of file #%s#\n",bprm->filename);
+	}
+#endif /*CONFIG_RKP_NS_PROT*/
 	retval = exec_mmap(bprm->mm);
 	if (retval)
 		goto out;
@@ -1923,6 +1996,18 @@ SYSCALL_DEFINE3(execve,
 		const char __user *const __user *, argv,
 		const char __user *const __user *, envp)
 {
+#ifdef CONFIG_RKP_KDP
+	struct filename *path = getname(filename);
+	int error = PTR_ERR(path);
+
+	if(IS_ERR(path))
+		return error;
+
+	if(rkp_cred_enable){
+		uh_call(UH_APP_RKP,0x4b,(u64)path->name,0,0,0);
+	}
+	putname(path);
+#endif
 	return do_execve(getname(filename), argv, envp);
 }
 
