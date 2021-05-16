ecb1e896a590c133bbff43c8a3bf3793b66b9d9a
Bruno Martins <bgcngm@gmail.com>
2021-05-16 12:27:32 +0100

    techpack: audio: Import Samsung changes from T725XXU2DUD1
    Change-Id: I7bd85724504c90b5cad64adb813882fc9b5d6280

--- techpack/audio/asoc/sdm660-common.h
+++ techpack/audio/asoc/sdm660-common.h
@@ -132,6 +132,15 @@ struct msm_asoc_mach_data {
 	int gpio_lineout_det;
 	int linein_det_swh;
 	int lineout_det_swh;
+#ifdef CONFIG_SAMSUNG_JACK
+	uint32_t amux_channel;
+	uint32_t moisture_channel;
+#endif /* CONFIG_SAMSUNG_JACK */
+	int dmic_ldo_en;
+	int fm_lna_en;
+#ifdef CONFIG_SND_SOC_WCD_MBHC_USB_ANALOG
+	int gnd_sel_gpio;
+#endif /* CONFIG_SND_SOC_WCD_MBHC_USB_ANALOG */
 };
 
 int msm_common_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
