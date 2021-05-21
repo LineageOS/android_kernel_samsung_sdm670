74338d2462db0f22296aaea88824d7c33109a1da
Bruno Martins <bgcngm@gmail.com>
2021-05-21 23:35:03 +0100

    security: Add KNOX sensitive data protection support
    Change-Id: I903dc7509cbb7dd21a37d0f3c61fa0f783eb7209

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -54,6 +54,10 @@
 #define CREATE_TRACE_POINTS
 #include <trace/events/ext4.h>
 
+#ifdef CONFIG_FSCRYPT_SDP
+#include <linux/fscrypto_sdp_cache.h>
+#endif
+
 void (*ufs_debug_func)(void *) = NULL;
 
 static struct ext4_lazy_init *ext4_li_info;
@@ -1039,6 +1043,12 @@ static struct inode *ext4_alloc_inode(struct super_block *sb)
 static int ext4_drop_inode(struct inode *inode)
 {
 	int drop = generic_drop_inode(inode);
+#ifdef CONFIG_FSCRYPT_SDP
+	if (!drop && fscrypt_sdp_is_locked_sensitive_inode(inode)) {
+		fscrypt_sdp_drop_inode(inode);
+		drop = 1;
+	}
+#endif
 
 	trace_ext4_drop_inode(inode, drop);
 	return drop;
