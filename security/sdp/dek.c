a602e03b871b035959cf7b265f36bf665e357566
LuK1337 <priv.luk@gmail.com>
2023-10-21 16:55:38 +0200

    treewide: sec: Address clang-17 build warnings
    Change-Id: I3f29a36cfc89fbb9ecc4b4bfbf5c05eac4408dd4

--- security/sdp/dek.c
+++ security/sdp/dek.c
@@ -102,7 +102,7 @@ int is_root(void) {
 	return 0;
 }
 
-int is_current_adbd() {
+int is_current_adbd(void) {
     DEK_LOGD("current->comm : %s\n", current->comm);
 #if 1
     if(is_root()) {
@@ -118,7 +118,7 @@ int is_current_adbd() {
 #endif
 }
 
-int is_current_epmd() {
+int is_current_epmd(void) {
     DEK_LOGD("current->comm : %s\n", current->comm);
 #if 1
     if(is_root()) {
